dc2978b5ba7d1f0530eb95c9f35cdd08a3fe8934
simjae383 <55373577+simjae383@users.noreply.github.com>
2022-06-07 13:22:44 +0900

    #3 로그아웃 기능 추가

--- app/src/main/java/com/sim981/a2022appointmentmanager/fragments/SettingsFragment.kt
+++ app/src/main/java/com/sim981/a2022appointmentmanager/fragments/SettingsFragment.kt
@@ -1,12 +1,17 @@
 package com.sim981.a2022appointmentmanager.fragments
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import com.sim981.a2022appointmentmanager.LoginActivity
 import com.sim981.a2022appointmentmanager.R
 import com.sim981.a2022appointmentmanager.databinding.FragmentSettingsBinding
+import com.sim981.a2022appointmentmanager.dialogs.CustomAlertDialog
+import com.sim981.a2022appointmentmanager.utils.ContextUtil
+import com.sim981.a2022appointmentmanager.utils.GlobalData
 
 class SettingsFragment : BaseFragment() {
     lateinit var binding : FragmentSettingsBinding
@@ -44,7 +49,25 @@ class SettingsFragment : BaseFragment() {
         }
 //        로그아웃 이벤트
         binding.logoutLayout.setOnClickListener {
+            val alert = CustomAlertDialog(mContext, requireActivity())
+            alert.myDialog()
 
+            alert.binding.dialogTitleTxt.text = "로그아웃"
+            alert.binding.dialogBodyTxt.text = "정말 로그아웃 하시겠습니까?"
+            alert.binding.dialogContentEdt.visibility = View.GONE
+            alert.binding.dialogPositiveBtn.setOnClickListener {
+                ContextUtil.clear(mContext)
+                GlobalData.loginUser = null
+
+                val myIntent = Intent(mContext, LoginActivity::class.java)
+                myIntent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                startActivity(myIntent)
+
+                alert.dialog.dismiss()
+            }
+            alert.binding.dialogNegativeBtn.setOnClickListener {
+                alert.dialog.dismiss()
+            }
         }
 //        회원 탈퇴 이벤트
         binding.secessionLayout.setOnClickListener {
