9e324923e3a47ee8458d90d344f39962457ae3c4
simjae383 <55373577+simjae383@users.noreply.github.com>
2022-06-07 12:20:14 +0900

    #3 메인 화면의 프래그먼트 4개(약속 목록, 친구 목록, 장소 목록, 설정) 작성

---
+++ app/src/main/java/com/sim981/a2022appointmentmanager/fragments/FriendsFragment.kt
@@ -0,0 +1,35 @@
+package com.sim981.a2022appointmentmanager.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.sim981.a2022appointmentmanager.R
+import com.sim981.a2022appointmentmanager.databinding.FragmentAppointmentsBinding
+
+class FriendsFragment : BaseFragment() {
+    lateinit var binding : FragmentAppointmentsBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_friends, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupEvents()
+        setValues()
+    }
+    override fun setupEvents() {
+
+    }
+
+    override fun setValues() {
+
+    }
+}
