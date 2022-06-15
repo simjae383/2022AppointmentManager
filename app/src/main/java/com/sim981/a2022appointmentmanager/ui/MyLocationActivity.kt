501ad75450d088bdd316d60a44d3b788ff269693
simjae383 <55373577+simjae383@users.noreply.github.com>
2022-06-15 11:36:30 +0900

    #8 현재 좌표 출력 성공

--- app/src/main/java/com/sim981/a2022appointmentmanager/ui/MyLocationActivity.kt
+++ app/src/main/java/com/sim981/a2022appointmentmanager/ui/MyLocationActivity.kt
@@ -1,20 +1,26 @@
 package com.sim981.a2022appointmentmanager.ui
 
 import android.Manifest
+import android.annotation.SuppressLint
 import android.location.Location
-import android.location.LocationRequest
+import android.location.LocationManager
 import android.os.Bundle
-
+import android.os.Looper
 import android.view.View
 import androidx.databinding.DataBindingUtil
-import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.*
 import com.gun0912.tedpermission.PermissionListener
 import com.gun0912.tedpermission.normal.TedPermission
 import com.sim981.a2022appointmentmanager.R
 import com.sim981.a2022appointmentmanager.databinding.ActivityMyLocationBinding
 
 class MyLocationActivity : BaseActivity() {
-    lateinit var binding : ActivityMyLocationBinding
+    lateinit var binding: ActivityMyLocationBinding
+
+    lateinit var mFusedLocationClient: FusedLocationProviderClient
+    private lateinit var mLocationManager: LocationManager
+    var latitude: Double = 0.0
+    var longitude: Double = 0.0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,27 +30,58 @@ class MyLocationActivity : BaseActivity() {
         setupEvents()
         setValues()
     }
+
     override fun onResume() {
         super.onResume()
     }
 
     override fun setupEvents() {
-        val pl = object : PermissionListener{
-            override fun onPermissionDenied(deniedPermissions: MutableList<String>?) {
-                finish()
-            }
+        binding.locationBtn.setOnClickListener {
+            val pl = object : PermissionListener {
+                override fun onPermissionDenied(deniedPermissions: MutableList<String>?) {
 
-            override fun onPermissionGranted() {
+                }
+
+                override fun onPermissionGranted() {
 //                지도 표시
+                    requestNewLocationData()
+                }
             }
+            TedPermission.create().setPermissionListener(pl)
+                .setPermissions(Manifest.permission.ACCESS_COARSE_LOCATION)
+                .setPermissions(Manifest.permission.ACCESS_FINE_LOCATION)
+                .setDeniedMessage("[설정] > [권한]에서 좌표 권한을 열어주세요.")
+                .check()
         }
-        TedPermission.create().setPermissionListener(pl)
-            .setPermissions(Manifest.permission.ACCESS_COARSE_LOCATION)
-            .setPermissions(Manifest.permission.ACCESS_FINE_LOCATION)
-            .setDeniedMessage("[설정] > [권한]에서 갤러리 권한을 열어주세요.")
-            .check()
     }
 
     override fun setValues() {
+    }
+
+    @SuppressLint("MissingPermission")
+    fun requestNewLocationData(){
+        var mLocationRequest = LocationRequest()
+        mLocationRequest.priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+        mLocationRequest.interval = 0
+        mLocationRequest.fastestInterval = 0
+        mLocationRequest.numUpdates = 1
+
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(mContext)
+        mFusedLocationClient.requestLocationUpdates(
+            mLocationRequest, mLocationCallback,
+            Looper.myLooper()!!
+        )
+    }
+    private val mLocationCallback = object : LocationCallback(){
+        override fun onLocationResult(locationResult: LocationResult) {
+            var mLastLocation : Location = locationResult.lastLocation
+            latitude = mLastLocation.latitude
+            longitude = mLastLocation.longitude
+            showMapView()
+        }
+    }
+
+    fun showMapView(){
+        binding.currentLatLngTxt.text = "위도 : ${latitude}, 경도 : $longitude"
     }
 }
